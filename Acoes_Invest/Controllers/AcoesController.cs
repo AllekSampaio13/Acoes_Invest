90382509b0e0cd9ec5d9f588fd2f15c576dae173
Állek Sampaio <alleksamp@gmail.com>
2025-10-30 15:45:11 -0300

    init: Iniciando projeto

---
+++ Acoes_Invest/Controllers/AcoesController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Acoes_Invest.Controllers
+{
+    public class AcoesController : Controller
+    {
+
+        [HttpGet("api/acoes/listar")]
+        public IActionResult ListarAcoes()
+        {
+            var acoes = new[]
+            {
+            new { Id = 1, Nome = "Ação A", Preco = 100.50 },
+            new { Id = 2, Nome = "Ação B", Preco = 200.75 },
+            new { Id = 3, Nome = "Ação C", Preco = 150.30 }
+        };
+            return Ok(acoes);
+        }
+
+    }
+}
+
